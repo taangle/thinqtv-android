9d38d6a97314236ff168d5cb0d5dfaa6fb0d572d
Hudson Pace <roliatica@gmail.com>
2020-02-11 23:44:12 -0700

    Initial commit.

---
+++ app/src/main/java/com/thinqtv/thinqtv_android/MainActivity.java
@@ -0,0 +1,50 @@
+package com.thinqtv.thinqtv_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        URL serverURL;
+        try {
+            serverURL = new URL("https://meet.jit.si");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Invalid server URL.");
+        }
+        JitsiMeetConferenceOptions defaultOptions
+                = new JitsiMeetConferenceOptions.Builder()
+                .setServerURL(serverURL)
+                .setWelcomePageEnabled(false)
+                .build();
+        JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+    }
+
+    public void onButtonClick(View v) {
+        EditText editText = findViewById(R.id.conferenceName);
+        String text = editText.getText().toString();
+        System.out.println(text);
+
+        if (text.length() > 0) {
+            JitsiMeetConferenceOptions options
+                    = new JitsiMeetConferenceOptions.Builder()
+                    .setRoom(text)
+                    .build();
+            JitsiMeetActivity.launch(this, options);
+        }
+    }
+}
