c4241cee16d5cac142f3167328e823229430e49e
Hudson Pace <hmpace@asu.edu>
2020-03-01 16:53:22 -0700

    Actually added the webview activity, missing from last commit

---
+++ app/src/main/java/com/thinqtv/thinqtv_android/WebViewActivity.java
@@ -0,0 +1,37 @@
+package com.thinqtv.thinqtv_android;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebViewActivity extends AppCompatActivity {
+    private WebView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        view = findViewById(R.id.webView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onLoadResource(WebView webView, String url) {
+                try {
+                    // remove the nav bar and the footer from each loaded page.
+                    view.loadUrl("javascript:(window.onload = function() { " +
+                            "(navBar = document.getElementsByTagName('nav')[0]); navBar.parentNode.removeChild(navBar);" +
+                            "(footer = document.getElementsByTagName('footer')[0]); footer.parentNode.removeChild(footer);" +
+                            "})()");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        view.loadUrl("http://thinq.tv/getinvolved");
+    }
+}
