2987a0fa04ed083d4baa04a28a7031ed9ff12af6
Anibal Bojorquez <abojorq5@asu.edu>
2020-03-07 15:27:22 -0700

    Added tool bar buttons along with web view.

--- app/src/main/java/com/thinqtv/thinqtv_android/GetInvolved.java
+++ app/src/main/java/com/thinqtv/thinqtv_android/GetInvolved.java
@@ -7,13 +7,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class GetInvolved extends AppCompatActivity {
+
+    WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_get_involve);
+        setContentView(R.layout.activity_web_view);
+
+        myWebView = findViewById(R.id.webView);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onLoadResource(WebView webView, String url) {
+                try {
+                    // remove the nav bar and the footer from each loaded page.
+                    myWebView.loadUrl("javascript:(window.onload = function() { " +
+                            "(navBar = document.getElementsByTagName('nav')[0]); navBar.parentNode.removeChild(navBar);" +
+                            "(footer = document.getElementsByTagName('footer')[0]); footer.parentNode.removeChild(footer);" +
+                            "})()");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        myWebView.setWebViewClient(new HelloWebViewClient());
+        myWebView.loadUrl("https://thinqtv.herokuapp.com/getinvolved");
     }
 
     public void goHome(View v){
@@ -25,4 +49,15 @@ public class GetInvolved extends AppCompatActivity {
         Intent i = new Intent(this, GetInvolvedApplyNowPage.class);
         startActivity(i);
     }
+
+    private class HelloWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+
 }
