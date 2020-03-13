a9f3591a392efa8417b74dc58f33137cd02d9c5c
Anibal Bojorquez <abojorq5@asu.edu>
2020-03-13 14:04:08 -0700

    Got rid of unused imports.

--- app/src/main/java/com/thinqtv/thinqtv_android/GetInvolved.java
+++ app/src/main/java/com/thinqtv/thinqtv_android/GetInvolved.java
@@ -7,13 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 public class GetInvolved extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private WebView view;
