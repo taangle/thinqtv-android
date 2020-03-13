f0c863a5b76c15e0f2a5680f4b5b4c2ce59ab1ae
Hudson Pace <hmpace@asu.edu>
2020-03-13 13:22:03 -0700

    App reacts to login properly, creates user if successful.

--- app/src/main/java/com/thinqtv/thinqtv_android/ui/login/LoginViewModel.java
+++ app/src/main/java/com/thinqtv/thinqtv_android/ui/login/LoginViewModel.java
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel;
 import android.content.Context;
 import android.util.Patterns;
 
+import com.thinqtv.thinqtv_android.data.LoginListener;
 import com.thinqtv.thinqtv_android.data.LoginRepository;
 import com.thinqtv.thinqtv_android.data.Result;
 import com.thinqtv.thinqtv_android.data.model.LoggedInUser;
@@ -29,24 +30,29 @@ public class LoginViewModel extends ViewModel {
     LiveData<LoginResult> getLoginResult() {
         return loginResult;
     }
+    public void setLoginResult(LoginResult result) {
+        loginResult.setValue(result);
+    }
 
     public void login(String username, String password, Context context) {
         // can be launched in a separate asynchronous job
-        Result<LoggedInUser> result = loginRepository.login(username, password, context);
+        LoginListener listener = new LoginListener() {
+            @Override
+            public void onError(Object response) {
 
-        if (result instanceof Result.Success) {
-            LoggedInUser data = ((Result.Success<LoggedInUser>) result).getData();
-            loginResult.setValue(new LoginResult(new LoggedInUserView(data.getName())));
-        } else {
-            loginResult.setValue(new LoginResult(R.string.login_failed));
-        }
+            }
+            public void onResponse(Object response) {
+
+            }
+        };
+        loginRepository.login(username, password, context, listener, this);
     }
 
     public void loginDataChanged(String username, String password) {
         if (!isUserNameValid(username)) {
-            loginFormState.setValue(new LoginFormState(R.string.invalid_username, null));
+            loginFormState.setValue(new LoginFormState(R.string.invalid_email, null));
         } else if (!isPasswordValid(password)) {
-            loginFormState.setValue(new LoginFormState(null, R.string.invalid_password));
+            loginFormState.setValue(new LoginFormState(null, R.string.password_empty));
         } else {
             loginFormState.setValue(new LoginFormState(true));
         }
@@ -54,18 +60,23 @@ public class LoginViewModel extends ViewModel {
 
     // A placeholder username validation check
     private boolean isUserNameValid(String username) {
+        return Patterns.EMAIL_ADDRESS.matcher(username).matches();
+        /*
         if (username == null) {
             return false;
+        }
+        if (username.contains("@")) {
+            return Patterns.E
         }
         if (username.contains("@")) {
             return Patterns.EMAIL_ADDRESS.matcher(username).matches();
         } else {
             return !username.trim().isEmpty();
-        }
+        }*/
     }
 
-    // A placeholder password validation check
     private boolean isPasswordValid(String password) {
-        return password != null && password.trim().length() > 5;
+        // return password != null && password.trim().length() >= 8; Should we validate password for login?
+        return password != null && password.trim().length() > 0;
     }
 }
