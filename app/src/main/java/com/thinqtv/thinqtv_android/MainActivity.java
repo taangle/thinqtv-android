d3f96fab9c5d8af67d7860a80d5fd8b71d5b99b2
Hudson Pace <hmpace@asu.edu>
2020-04-08 19:22:05 -0700

    Clean files for merge.

--- app/src/main/java/com/thinqtv/thinqtv_android/MainActivity.java
+++ app/src/main/java/com/thinqtv/thinqtv_android/MainActivity.java
@@ -21,7 +21,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
@@ -166,7 +165,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // listener for when a user clicks an event to go to its page
-    private class ViewEventDetails_ClickListener implements View.OnClickListener{
+    private class ViewEventDetails_ClickListener implements View.OnClickListener {
         private Context mContext;
         private String eventCode;
 
