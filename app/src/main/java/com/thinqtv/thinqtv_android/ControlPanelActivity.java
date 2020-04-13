a2af382695efc1e4bc0a3d1b75ad2dc0ad62066c
Hudson Pace <hmpace@asu.edu>
2020-04-13 13:03:15 -0700

    Add basic control panel activity.

---
+++ app/src/main/java/com/thinqtv/thinqtv_android/ControlPanelActivity.java
@@ -0,0 +1,14 @@
+package com.thinqtv.thinqtv_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ControlPanelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_panel);
+    }
+}
