43ffb0f34b789693695e651333525632c4c76df5
Hudson Pace <hmpace@asu.edu>
2020-04-24 04:07:15 -0700

    create pickers for date and time.

--- app/src/main/java/com/thinqtv/thinqtv_android/AddEventActivity.java
+++ app/src/main/java/com/thinqtv/thinqtv_android/AddEventActivity.java
@@ -1,11 +1,14 @@
 package com.thinqtv.thinqtv_android;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -15,31 +18,65 @@ import java.util.Calendar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
-public class AddEventActivity extends AppCompatActivity {
+public class AddEventActivity extends AppCompatActivity{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
-        TextView setTimeButton = findViewById(R.id.time);
+        Button setTimeButton = findViewById(R.id.time);
         setTimeButton.setOnClickListener(view -> {
-            DialogFragment newFragment = new TimePickerFragment();
+            DialogFragment newFragment = new TimePickerFragment(new TimePickerDialog.OnTimeSetListener() {
+                public void onTimeSet(TimePicker view, int hour, int minute) {
+                    String msg = hour + ":" + minute;
+                    setTimeButton.setText(msg);
+                }
+            });
             newFragment.show(getSupportFragmentManager(), "timePicker");
+        });
+        Button setDateButton = findViewById(R.id.date);
+        setDateButton.setOnClickListener(view -> {
+            DialogFragment newFragment = new DatePickerFragment(new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    String msg = day + " " + month + ", " + year;
+                    setDateButton.setText(msg);
+                }
+            });
+            newFragment.show(getSupportFragmentManager(), "datePicker");
         });
     }
 
-    public static class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener{
+    public static class TimePickerFragment extends DialogFragment {
+        private TimePickerDialog.OnTimeSetListener listener;
+        public TimePickerFragment(TimePickerDialog.OnTimeSetListener listener) {
+            super();
+            this.listener = listener;
+        }
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             // default values are the current time
             final Calendar c = Calendar.getInstance();
             int hour = c.get(Calendar.HOUR_OF_DAY);
             int minute = c.get(Calendar.MINUTE);
-            return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
+            return new TimePickerDialog(getActivity(), listener, hour, minute, DateFormat.is24HourFormat(getActivity()));
         }
+    }
 
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            // do... something?
+    public static class DatePickerFragment extends DialogFragment {
+        private DatePickerDialog.OnDateSetListener listener;
+        public DatePickerFragment(DatePickerDialog.OnDateSetListener listener) {
+            super();
+            this.listener = listener;
+        }
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // default values are the current date
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            return new DatePickerDialog(getActivity(), listener, year, month, day);
         }
     }
 }
