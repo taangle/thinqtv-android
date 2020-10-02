f70a4114320767d173df5d13ef4a77523b2fbf3d
Anibal Bojorquez <abojorq5@asu.edu>
2020-10-02 16:04:26 -0700

    Adding about_us fragment files

---
+++ app/src/main/java/com/thinqtv/thinqtv_android/data/model/AboutUsModel.java
@@ -0,0 +1,13 @@
+package com.thinqtv.thinqtv_android.data.model;
+
+public class AboutUsModel {
+    public String Title = "";
+
+    public String DetailsSection1Title = "";
+    public String DetailsSection1Content = "";
+
+    public String DetailsSection2Title = "";
+    public String DetailsSection2Content = "";
+
+    public String[] ImageURLs = {""};
+}
