42178d368516a1d50904874cce77990049a8edce
Anibal Bojorquez <abojorq5@asu.edu>
2020-10-29 23:53:23 -0700

    Added elements from the site to the fragment using jsoup.

---
+++ app/src/main/java/com/thinqtv/thinqtv_android/data/model/InviteUsModel.java
@@ -0,0 +1,8 @@
+package com.thinqtv.thinqtv_android.data.model;
+
+public class InviteUsModel {
+    public String Title = "";
+
+    public String MessageTitle1 = "";
+    public String Message1 = "";
+}
